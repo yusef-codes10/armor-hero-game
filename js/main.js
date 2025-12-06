a3c4e911831d91dd8ec9e4caf850c5c47057f008
Youcef <youcefcopy@gmail.com>
2025-12-06 22:40:38 +0100

    testing the btns

--- js/main.js
+++ js/main.js
@@ -2,3 +2,19 @@ console.log('main loaded');
 const newGameBtn = document.getElementById('newGameBtn');
 const settingsBtn = document.getElementById('settingsBtn');
 const exitBtn = document.getElementById('exitBtn');
+
+const mainMenu = document.querySelector('main-menu');
+
+settingsBtn.addEventListener('click', openSettingsWindow);
+
+function openNewGameWindow() {
+    
+}
+
+function openSettingsWindow() {
+    mainMenu.classList.toggle('hidden');
+}
+
+function openExitWindow() {
+    
+}
