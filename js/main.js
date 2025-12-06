6fc779268ed112f7a7fd8c6d5db29bd2ba4c0808
Youcef <youcefcopy@gmail.com>
2025-12-06 22:34:38 +0100

    declaring the dom bts

--- js/main.js
+++ js/main.js
@@ -1,1 +1,4 @@
 console.log('main loaded');
+const newGameBtn = document.getElementById('newGameBtn');
+const settingsBtn = document.getElementById('settingsBtn');
+const exitBtn = document.getElementById('exitBtn');
