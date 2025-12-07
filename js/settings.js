f4b9ab24276b77928266e4b479bc9ee26bbd8803
Youcef <youcefcopy@gmail.com>
2025-12-07 10:33:14 +0100

    fight theme added

--- js/settings.js
+++ js/settings.js
@@ -7,6 +7,7 @@ stopBtn.addEventListener('click', stopMainTheme);
 
 // music player is here
 const mainTheme = new Audio('../assets/sound/main-theme.mp3');
+const fightTheme = new Audio('../assets/sound/fight-theme.mp3');
 
 function playMainTheme() {
     mainTheme.play();
