52d740d0067e6ded91117a49b7e4114dd362f100
Youcef <youcefcopy@gmail.com>
2025-12-07 10:20:52 +0100

    main theme player is working

--- js/settings.js
+++ js/settings.js
@@ -1,3 +1,12 @@
 console.log('settings loaded');
 const playBtn = document.getElementById('playBtn');
 const stopBtn = document.getElementById('stopBtn');
+
+playBtn.addEventListener('click', playMainTheme);
+
+// music player is here
+const mainTheme = new Audio('../assets/sound/main-theme.mp3');
+
+function playMainTheme() {
+    mainTheme.play();
+}
