777564b16091af8b6eb34bbf2bf9144222d7918c
Youcef <youcefcopy@gmail.com>
2025-12-07 10:23:29 +0100

    pause method is working

--- js/settings.js
+++ js/settings.js
@@ -3,10 +3,15 @@ const playBtn = document.getElementById('playBtn');
 const stopBtn = document.getElementById('stopBtn');
 
 playBtn.addEventListener('click', playMainTheme);
+stopBtn.addEventListener('click', stopMainTheme);
 
 // music player is here
 const mainTheme = new Audio('../assets/sound/main-theme.mp3');
 
 function playMainTheme() {
     mainTheme.play();
+}
+
+function stopMainTheme() {
+    mainTheme.pause();
 }
