42839b3276fed6045a4b2481f2dfcff8bd3331f5
Youcef <youcefcopy@gmail.com>
2025-12-07 11:43:45 +0100

    another bug fixed | the condition of out of bounds

--- js/settings.js
+++ js/settings.js
@@ -10,9 +10,10 @@ nextBtn.addEventListener('click', palyNextTheme);
 // music player is here
 const mainTheme = new Audio('../assets/sound/main-theme.mp3');
 const fightTheme = new Audio('../assets/sound/fight-theme.mp3');
+const romanceTheme = new Audio('../assets/sound/romance-theme.mp3');
 
 // declaring the themes array
-const themes = [mainTheme, fightTheme];
+const themes = [mainTheme, fightTheme, romanceTheme];
 console.log(themes);
 
 let currentTheme; // a var to hold the music to play
@@ -33,11 +34,12 @@ function stopMainTheme() {
 
 function palyNextTheme() {
     // ! have to secure getting out of bounds
-    if (currentThemeIndex >= themes.length) {
-        currentThemeIndex = 0;
+    if (currentIndex < themes.length - 1) { // bset logic, cuz you will still be incrementing too far when the index is in range but less then the themes length
+        currentIndex++;
     } else {
-        currentThemeIndex++; // fix jumping too far
+        currentIndex = 0;
     }
+    stopMainTheme();
     currentTheme = themes[currentThemeIndex];
     console.log(currentThemeIndex);
     console.log(currentTheme);
